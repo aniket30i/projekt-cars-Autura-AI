dd43a6bc8de2feaf2ea16c901b2c1f766314d7f6
aniket30i <iamaniket.saha.09@protonmail.com>
2025-06-21 02:26:28 +0530

    assets upload

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      Hello
-      <br />
-      <Button variant="destructive">Hi</Button>
-    </div>
-  );
+  return <div>.</div>;
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,19 +5,63 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { CarFront, Heart, Layout } from "lucide-react";
 
-const Header = () => {
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
   return (
-    <div>
-      <SignedOut>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      {/* <SignedOut>
         <SignInButton />
         <SignUpButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
-      </SignedIn>
-    </div>
+      </SignedIn> */}
+      <nav className="mx-auto px-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"}>
+          <Image
+            src={"/logo.png"}
+            alt="Projekt Cars"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdmin && (
+            <span className="text-xs font-extralight">Hello this is admin</span>
+          )}
+        </Link>
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href="/saved-cars">
+              <Button>
+                <Heart size={18} />
+                <span className="hidden md:inline">Saved Cars</span>
+              </Button>
+            </Link>
+            {!isAdmin ? (
+              <Link href="/reservations">
+                <Button variant="outline">
+                  <CarFront size={18} />
+                  <span className="hidden md:inline">My Reservations</span>
+                </Button>
+              </Link>
+            ) : (
+              <Link href="/admin">
+                <Button variant="outline">
+                  <Layout size={18} />
+                  <span className="hidden md:inline">Admin Portal</span>
+                </Button>
+              </Link>
+            )}
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
