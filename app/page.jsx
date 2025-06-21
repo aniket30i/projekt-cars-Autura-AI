71c6aedf7bdd823a394c0a6cdbf6d2d9c26674e9
aniket30i <iamaniket.saha.09@protonmail.com>
2025-06-22 01:42:15 +0530

    landing page

---
+++ app/page.jsx
@@ -0,0 +1,44 @@
+import CarCard from "@/components/CarCard";
+import HomeSearch from "@/components/home-search";
+import { Button } from "@/components/ui/button";
+import { featuredCars } from "@/lib/data";
+import { ChevronRight } from "lucide-react";
+
+export default function Home() {
+  console.log(featuredCars);
+  return (
+    <div className="pt-20 flex flex-col ">
+      {/*Hero */}
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="mb-8 ">
+            <h1 className="text-5xl md:text-8xl mb-4 gradient-title">
+              Find Your Dream Car with Projekt Cars AI
+            </h1>
+            <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
+              Advanced AI Car Search and test drive from thousands of vehicles
+            </p>
+          </div>
+          {/* Search */}
+          <HomeSearch />
+        </div>
+      </section>
+      <section className="py-12">
+        <div>
+          <div>
+            <h2>Featured Cars</h2>
+            <Button>
+              View All
+              <ChevronRight className="ml-1 h-4 w-4" />
+            </Button>
+            <div>
+              {featuredCars.map((car) => {
+                return <CarCard key={car.id} car={car} />;
+              })}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
